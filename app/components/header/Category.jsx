19115a2a66e183df037b11f7af19d2f8cfce04aa
hasti sajadi <109924837+HastiSajadi@users.noreply.github.com>
2023-12-08 21:49:06 +0330

    category fix bugs

--- app/components/header/Category.jsx
+++ app/components/header/Category.jsx
@@ -11,16 +11,15 @@ export default function Category(){
     
     return(
         <>
-         <div className="container-fluid bg-dark py-1 px-2 d-flex flex-nowrap">
+         <div className="container-fluid py-1 px-2 d-flex flex-row-reverse justify-content-between flex-nowrap">
            {
-            Api.map( (index,{id,title,icon}) =>{
+            Api.map( ({id,title,icon}) =>{
             return(
                 <div key={id} className={clsx(style.categoryItems)}>
                 <img className={clsx(style.categoryItemsImg , "mb-2")} src={icon} alt="foods" />
                 <p className="m-0">{title}</p>
             </div>
             )
-
             })
            }
          </div>
