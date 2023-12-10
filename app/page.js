6fef886dab7314302dc6351652649382773278bb
hasti sajadi <109924837+HastiSajadi@users.noreply.github.com>
2023-12-10 19:33:10 +0330

    fix bugs

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import FoodCategory from './components/food category/FoodCategory';
-import FoodParty from './components/foodParty/FoodParty';
 import "./page.module.css"
 
 
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <>
      <FoodCategory/>
-     <FoodParty/>
     </>
   )
 }
