aeb5d319d68e928fb20484adda9648b6b5abfa4d
atieh-mir <atiehmir17@gmail.com>
2023-12-10 06:17:44 +0330

    route

--- navigation/ButtonLogin.jsx
+++ navigation/ButtonLogin.jsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation"
 export default function ButtonLogin({children}){
     const routerbutton = useRouter();  
     return(
-        <button onClick={() => routerbutton.push('/login')} > {children} </button>
+        <button onClick={() => routerbutton.push('/')} > {children} </button>
     )
 
 }
