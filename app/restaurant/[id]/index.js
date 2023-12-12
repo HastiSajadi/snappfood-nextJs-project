a4dc3db0d88307feea06fbb08c54842acee9fb09
atieh-mir <atiehmir17@gmail.com>
2023-12-13 00:26:17 +0330

    unitial commit

--- app/restaurant/[id]/index.js
+++ app/restaurant/[id]/index.js
@@ -3,9 +3,8 @@
 import pizza from "../../../api/foods/pizzas.json";
 import burgers from "../../../api/foods/burgers.json";
 import drinks from "../../../api/foods/drinks.json";
-import fries from "../../../api/foods/fries.json";
 import hotdog from "../../../api/foods/hotdog.json";
-import salads from "../../../api/foods/salads.json";
+import salads_fries from "../../../api/foods/salads_fries.json";
 import steaks from "../../../api/foods/steaks.json";
 
 
@@ -19,8 +18,7 @@ export{
     pizza,
     burgers,
     drinks,
-    fries,
     hotdog,
-    salads,
+    salads_fries,
     steaks
 }
