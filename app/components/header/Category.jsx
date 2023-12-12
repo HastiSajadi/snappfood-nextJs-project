a650227e3384147674eac50c7815eb30b9ce213b
atieh-mir <atiehmir17@gmail.com>
2023-12-12 14:35:43 +0330

    category

--- app/components/header/Category.jsx
+++ app/components/header/Category.jsx
@@ -1,9 +1,9 @@
+'use client'
+
 import style from "./css/category.module.css"
 import clsx from "clsx";
 import Api from "../../../api/categories/categories.json"
-
-
- 
+import Link from "next/link";
 
 export default  function Category(){
     return(
@@ -12,10 +12,12 @@ export default  function Category(){
            {
             Api.map( ({id,title,icon}) =>{
             return(
-                <div key={id} className={clsx(style.categoryItems)}>
-                <img className={clsx(style.categoryItemsImg , "mb-2")} src={icon} alt="foods" />
-                <p className="m-0">{title}</p>
+                <Link href={'./restaurant'}  key={id} >
+                <div  className={clsx(style.categoryItems)}>
+               <img className={clsx(style.categoryItemsImg , "mb-2")} src={icon} alt="foods" />
+                <p className="m-0">{title}   </p>
             </div>
+            </Link>
             )
             })
            }
