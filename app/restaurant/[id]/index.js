f0613a11f622674c3d2225f007796013d0c39dde
atieh-mir <atiehmir17@gmail.com>
2023-12-13 10:30:25 +0330

    initial commit

--- app/restaurant/[id]/index.js
+++ app/restaurant/[id]/index.js
@@ -8,7 +8,12 @@ import salads_fries from "../../../api/foods/salads_fries.json";
 import steaks from "../../../api/foods/steaks.json";
 
 
-
+export * from "./Burger";
+export * from "./Pizza";
+export * from "./HotDog";
+export * from "./Steak";
+export * from "./Appetizer";
+export * from "./Drinks";
 
 
 
