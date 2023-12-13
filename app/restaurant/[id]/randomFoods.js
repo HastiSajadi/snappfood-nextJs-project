34dda8e459e2aa31fd57464dc496fc806a745485
atieh-mir <atiehmir17@gmail.com>
2023-12-13 10:30:54 +0330

    random foods

--- app/restaurant/[id]/randomFoods.js
+++ app/restaurant/[id]/randomFoods.js
@@ -12,9 +12,9 @@ const salad = salads_fries.sort(() => 0.5 - Math.random());
 const steak = steaks.sort(() => 0.5 - Math.random());
 
 // Get sub-array of first n elements after shuffled
-let selectedPizzas = pizzas.slice(0, 3);
-let selectedBurger = burger.slice(0, 3);
-let selectedDrink = drink.slice(0, 3);
+let selectedPizzas = pizzas.slice(0, 4);
+let selectedBurger = burger.slice(0, 4);
+let selectedDrink = drink.slice(0, 4);
 let selectedHotdogs = hotdogs.slice(0, 3);
 let selectedSalads = salad.slice(0, 5);
 let selectedSteak = steak.slice(0, 3);
