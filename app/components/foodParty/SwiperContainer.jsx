948b98fff69858ef24f52696ca1912b414f31593
hasti sajadi <109924837+HastiSajadi@users.noreply.github.com>
2023-12-13 18:44:28 +0330

    add the detail and style to the modal

--- app/components/foodParty/SwiperContainer.jsx
+++ app/components/foodParty/SwiperContainer.jsx
@@ -1,23 +1,23 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import '../../../node_modules/swiper/swiper.css';
 import Api from "../../../api/foodParty/foodParty.json"
+// import Api2 from "../../../api/foodParty/usersComment.json"
 import clsx from "clsx"
 import style from "./css/foodparty.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faStar} from "@fortawesome/free-solid-svg-icons"
+import {faStar,faClock,faChevronLeft} from "@fortawesome/free-solid-svg-icons"
 import { Navigation, A11y } from 'swiper/modules';
 import '../../../node_modules/swiper/modules/navigation.css';
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 
 export default function SwiperContainer(){
 
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
     return (
       <Swiper
       breakpoints={{
@@ -47,7 +47,7 @@ export default function SwiperContainer(){
         onSwiper={(swiper) => console.log(swiper)}
       >
         {
-            Api.map(({id , title , img , restaurant , deliveryPrice , price , deal , rate , count})=>{
+            Api.map(({id , title ,des , img , restaurant , deliveryPrice , price , deal , rate , count})=>{
                 return(
                     <SwiperSlide >
                     <div key={id} onClick={handleShow} className={clsx("bg-white  text-dark rounded-4 d-flex flex-column  p-5 align-items-center " , style.cards)}>
@@ -79,33 +79,97 @@ export default function SwiperContainer(){
                         </div>
                     </div>
                 </div>
+
+
+                <Modal size="lg" show={show} onHide={handleClose}>
+                  <div className={clsx('d-flex justify-content-between px-2 rounded-top-2',style.modalHead)} >
+                    <h4 className="my-3 h6">
+                        <FontAwesomeIcon className="me-2" icon={faClock} />
+                        12 : 45 : 20
+                    </h4>
+                    <div className='d-flex align-items-center'>
+                      <h5 className="">فود پارتی</h5>
+                      <img className={style.sparkles} src="https://s6.uupload.ir/files/sparkles-svgrepo-com_uzt9.png" />
+                    </div>
+                  </div>
+                  <Modal.Header closeButton></Modal.Header>
+                  <Modal.Body>
+                    <div className={clsx('container-fluid p-2', style.modalBody)} >
+                      <div className='row d-flex justify-content-end'>
+                        <div className='col-7 d-flex flex-column mt-2 '>
+                          <div className='d-flex mb-3 justify-content-between w-100'>
+                            <div >
+                              <span className=" px-1 border-opacity-10 border rounded-1">
+                              {rate}<FontAwesomeIcon className="text-warning  ms-1" icon={faStar} />
+                              </span>
+                            </div> 
+                            <h4 className={clsx("fw-bold")}>{title}</h4>
+                          </div>
+                          <p className='text-end text-black-50'>{des}</p>
+                          <div className='d-flex w-100 justify-content-end align-items-end'>
+                            <span className="text-secondary">عدد باقی مانده</span>
+                            <p className='m-0 ms-1 h5 fw-bold'>{count} </p>
+                          </div>
+                          <div className='w-100 border border-2 border-secondary-subtle mt-3'></div>
+                          <div className='d-flex w-100 my-3 justify-content-between'>
+                          <button type='submit' className={style.modalSubmitBtn} >افزودن</button>
+                          <div>
+                            <div className="d-flex mt-2 align-items-center">
+                              <div className='d-flex flex-column'>
+                                <span className={style.dealText}>{price}</span>
+                                <span className='fw-bold'>{price}</span>
+                              </div>
+                              <div><p className={clsx("badge p-2 ms-2 mb-2" , style.modalBadge)}>{deal}</p></div>
+                              
+                            </div>
+                          </div>
+                          </div>
+                          <div className='d-flex justify-content-between mt-4'>
+                            <a className='text-success fw-bold text-decoration-none'> <FontAwesomeIcon className='me-2' icon={faChevronLeft}/>{restaurant} </a>
+                            <p className='h6'>ارسال اکسپرس</p>
+                          </div>
+                        </div>
+                        <div className='col-5 d-flex'>
+                        <img className="w-100 rounded my-3" alt="food" src={img} />
+                        </div>
+                      </div>
+                      {/* بخش نظرات کاربران */}
+                      <div className='w-100 d-flex flex-column align-items-end'>
+                              <p className={clsx(style.commentSecTitle)}>نظرات کاربران</p>
+                              {/* {
+                                Api2.map((date,names,rate,text,id)=>{
+                                  return(
+                                    <div key={id}>
+                                      <div className='w-100 border-bottom border-black-50 p-2 d-flex'>
+                                      <div className='text-end me-5'>
+                                        <p>{text}</p>
+                                    </div>
+                                    <div className='d-flex flex-column'>
+                                      <h4>{names}</h4>
+                                      <p>{date}</p>
+                                      <span className="mb-2">
+                                        <FontAwesomeIcon className="text-warning me-1" icon={faStar} />
+                                        {rate}
+                                      </span>
+                                    </div>
+                                
+                                      </div>
+                                    </div>
+                                  )
+                                })
+                              } */}
+                      </div>
+                    </div>
+                  </Modal.Body>
+
+                </Modal>
+
+
                 </SwiperSlide>
                 )
             })
         }
-        <Modal size="lg" show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title><img className={clsx(style.logoImg)} src="https://i.ibb.co/FhQJQDd/snappfood-logo.png"/></Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <div className={clsx("d-flex flex-column p-3  align-items-end" , style.modalBody)}>
-          <p className="h5">
-            <span className="h3">ورود</span>
-            <span className="mx-2">یا</span>
-            <span className="h3">عضویت</span>
-          </p>
-
-          <p className="mb-2 mt-4 text-secondary">شماره تلفن همراه</p>
-          <input className={style.numberInput} type="text" />
-          <p className="mb-2 mt-2 text-secondary">شماره با ۰۹ شروع می‌شود</p>
-          </div>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button className={clsx("disabled border-0 w-100 p-3 text-dark fw-bold" , style.submitBtn)} onClick={handleClose}>
-            ادامه 
-          </Button>
-        </Modal.Footer>
-      </Modal>
+        
       </Swiper>
       
     );
