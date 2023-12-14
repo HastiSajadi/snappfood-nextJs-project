19b2ff9689e5beca2b0cba1bae813c6d465a5998
atieh-mir <atiehmir17@gmail.com>
2023-12-14 23:30:14 +0330

    request for sellers

---
+++ app/components/Sellers/Sellers.jsx
@@ -0,0 +1,22 @@
+
+
+export const Sellers = () => {
+
+
+    return (
+       <>
+       <div>
+        <h1>
+        صاحب کسب و کار هستید؟
+        </h1>
+        <h4>
+        با اسنپ فود کسب و کارتان را آنلاین کنید و فروشتان را افزایش دهید.
+        </h4>
+        <button> ثبت نام فروشندگان </button>
+       </div>
+       <div>
+        <img src="https://snappfood.ir/static/images/vendor_pic.png" />
+       </div>
+       </>
+    )
+}
