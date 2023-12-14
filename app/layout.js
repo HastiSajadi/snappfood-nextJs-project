0e0093cd610e93ff374428f7662d15d39de86457
hasti sajadi <109924837+HastiSajadi@users.noreply.github.com>
2023-12-14 16:24:52 +0330

    .

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({ children }) {
         {pathname === "/"  && <Header/>}
         {pathname === "/restaurant" && <Header/>}
 
-        {children}======={children}
+        {children}
         </body>
     </html>
   )
