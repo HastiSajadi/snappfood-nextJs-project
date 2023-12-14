8959097191a73b0cf2a968ab65bca9f591e95303
hasti sajadi <109924837+HastiSajadi@users.noreply.github.com>
2023-12-14 19:11:04 +0330

    nav category bug fixed

--- app/components/header/Category.jsx
+++ app/components/header/Category.jsx
@@ -8,10 +8,16 @@ export default  function Category(){
     return(
         <>
          <div className={clsx("container-fluid py-1 border-bottom px-2 d-flex flex-row-reverse justify-content-between flex-nowrap" , style.categoryContainer)}>
+            <Link className="text-decoration-none" href={'./restaurant'} >
+                <div  className={clsx(style.categoryItems)}>
+                <img className={clsx(style.categoryItemsImg , "mb-2")} src="https://cdn.snappfood.ir/uploads/images/review-app/icons/count/desktop_1_603508bf202d8_img_st_food.png" alt="foods" />
+                <p className="m-0 text-dark ">رستوران</p>
+                </div>
+            </Link>
            {
             Api.map( ({id,title,icon}) =>{
             return(
-                <Link className="text-decoration-none" href={'./restaurant'}  key={id} >
+                <Link className="text-decoration-none" href={'./'}  key={id} >
                 <div  className={clsx(style.categoryItems)}>
                <img className={clsx(style.categoryItemsImg , "mb-2")} src={icon} alt="foods" />
                 <p className="m-0 text-dark ">{title}   </p>
