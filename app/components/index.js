c5720aac403dfdb7f64024c0d82c85dab681cb42
atieh-mir <atiehmir17@gmail.com>
2023-12-14 20:25:27 +0330

    initial commit

---
+++ app/components/index.js
@@ -0,0 +1,9 @@
+
+export * from "./Bests/Bests";
+export * from "./food category/FoodCategory";
+export * from "./foodParty/FoodParty";
+export * from "./Gifts/Gifts";
+export * from "./NewsInSnapp/NewsInSnapp";
+export * from "./Options/Options";
+export * from "./SpecialTastes/SpecialTastes";
+export * from "./UsersSuggestion/UsersSuggestion";
