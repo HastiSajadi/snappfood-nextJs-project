6fd3c26c8016d98ba0eb9cfbe21df029e76d596d
atieh-mir <atiehmir17@gmail.com>
2023-12-14 19:17:12 +0330

    gifts

--- app/components/Gifts/Gifts.jsx
+++ app/components/Gifts/Gifts.jsx
@@ -3,6 +3,7 @@ import style from "../restaurantsCard.module.css"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faChevronLeft,faStar} from "@fortawesome/free-solid-svg-icons"
 import {RestaurantsComponents} from "../RestaurantsComponents.jsx"
+import Link from "next/link"
 
 // import Something from "./restaurantCarousel"
 
@@ -14,7 +15,7 @@ export default function Gifts(){
         <>
         <div className={clsx("container-fluid d-flex flex-column align-items-end my-5 ", style.container)}>
           <div className="d-flex justify-content-between px-4 w-100">
-          <a className='text-success fw-bold h5 text-decoration-none'> <FontAwesomeIcon className='me-2' icon={faChevronLeft}/>مشاهده همه</a>
+          <Link href="/restaurant"  className='text-success fw-bold h5 text-decoration-none'> <FontAwesomeIcon className='me-2' icon={faChevronLeft}/>مشاهده همه</Link>
           <h3 className="fw-bold text-dark-emphasis">جایزه خرید</h3>
           </div>
           <RestaurantsComponents />
