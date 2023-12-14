6a81fb2c9fb17c665580417f23e23f5997d4f726
atieh-mir <atiehmir17@gmail.com>
2023-12-14 20:25:18 +0330

    Home page

--- app/page.js
+++ app/page.js
@@ -1,17 +1,9 @@
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Bests from './components/Bests/Bests';
-import FoodCategory from './components/food category/FoodCategory';
-import FoodParty from './components/foodParty/FoodParty';
-import Gifts from './components/Gifts/Gifts';
-import NewsInSnapp from './components/NewsInSnapp/NewsInSnapp';
-import Options from './components/Options/Options';
-import SpecialTastes from './components/SpecialTastes/SpecialTastes';
-import UsersSuggestion from './components/UsersSuggestion/UsersSuggestion';
 import style from "./page.module.css"
 import dynamic from "next/dynamic"
-
+import { Bests, FoodCategory, FoodParty,Gifts,NewsInSnapp,Options ,SpecialTastes,UsersSuggestion } from './components'
 export const metadata = {
   title: 'اسنپ فود | سفارش آنلاین غذا از تمامی رستوران ها  و فست فود ها',
   description: 'snapp food',
