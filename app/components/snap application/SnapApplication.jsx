6161ac4a8880cb43e7c12d897a78b6a831ee047d
atieh-mir <atiehmir17@gmail.com>
2023-12-14 21:30:18 +0330

    snapp Aplication

--- app/components/snap application/SnapApplication.jsx
+++ app/components/snap application/SnapApplication.jsx
@@ -5,8 +5,25 @@ export const SnapApplication = () => {
 
 
     return(
+       
         <div>
-            salam
+       <div>
+      <div>
+      <h1>
+        اپلیکیشن اسنپ‌فود
+        </h1>
+        <p>
+        با اپلیکیشن اسنپ‌فود به راحتی و با چند کلیک ساده می‌توانید رستوران‌ها، کافه‌ها، شیرینی‌فروشی‌ها و سوپرمارکت‌های نزدیک خودتان را جست‌و‌جو کرده و از تجربه سفارش آسان از اسنپ‌فود لذت ببرید.
+        </p>
+      </div>
+      <div>
+        <label> برای دریافت لینک دانلود اپلیکیشن، شماره موبایلتان رو وارد کنید </label>
+        <input placeholder="09*********"/> <button>دریافت کد</button>
+      </div>
+       </div>
+       <div>
+        <img src="https://snappfood.ir/static/images/img_app_mockup@2x.png"/>
+       </div>
         </div>
     )
 }
