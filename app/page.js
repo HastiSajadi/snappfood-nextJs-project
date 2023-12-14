87f4170ff31ad99673ee4ce6c9ceeaee0a624dc5
atieh-mir <atiehmir17@gmail.com>
2023-12-14 17:50:12 +0330

    home

--- app/page.js
+++ app/page.js
@@ -3,6 +3,10 @@ import FoodCategory from './components/food category/FoodCategory';
 import FoodParty from './components/foodParty/FoodParty';
 import style from "./page.module.css"
 
+export const metadata = {
+  title: 'اسنپ فود | سفارش آنلاین غذا از تمامی رستوران ها  و فست فود ها',
+  description: 'snapp food',
+}
 
 export default function Home() {
   return (
