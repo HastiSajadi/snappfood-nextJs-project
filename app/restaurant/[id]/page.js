7c84949a482de9b9e75c4db0c9876b5a09071c99
hasti sajadi <109924837+HastiSajadi@users.noreply.github.com>
2023-12-19 21:32:12 +0330

    add breadcrums to menu page

--- app/restaurant/[id]/page.js
+++ app/restaurant/[id]/page.js
@@ -10,9 +10,27 @@ import style from "./page.module.css"
 
 export default   function Menu(){
    
+    const breadcrum = [
+        {id:0, name: "اسنپ فود"},
+        {id:1, name: "رستوران ها"},
+        {id:1, name: "فست فود"}
+    ]
+
     return(
         <div>
           <Navbar/>
+
+          <main className={style.main} >
+          <BreadCrums>
+            {
+              breadcrum.map(bread=>
+                <li className={style.breadItem} key={bread.id}>
+                  <p className={clsx("m-0 " , style.breadFontsize)}>{bread.name}</p>
+                </li>
+              )
+            }
+          </BreadCrums>
+          </main>
         </div>
     )
 
