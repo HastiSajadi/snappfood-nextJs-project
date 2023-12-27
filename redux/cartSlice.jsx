a164432bbadb69a95f6a1764cfeb5407eaf2b873
atieh-mir <atiehmir17@gmail.com>
2023-12-27 20:47:53 +0330

    reducer

--- redux/cartSlice.jsx
+++ redux/cartSlice.jsx
@@ -14,77 +14,97 @@ const setStorLocal = (item, value) => {
 }
 
 
-const initialState = {
-   cart:getStorLocal("cart")
-     ? JSON.parse(getStorLocal("cart"))
-     : [],
-   cartTotalQuantity: 0,
-   cartTotalAmount: 0,
-  
- };
- 
+ const initialState = {
+  cartItems: getStorLocal("cart")
+    ? JSON.parse(getStorLocal("cart"))
+    : [],
+  cartTotalQuantity: 0,
+  cartTotalAmount: 0,
+};
 
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addToCart(state, action) {
+      const existingIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
 
- export const carttSlice = createSlice({
-   name:"cart",
-   initialState,
-   reducers:{
-     addToCart(state,action){
-      const itemIndex = state.cart.findIndex((item) => item.id === action.payload.id );
-      if(itemIndex>= 0){
-         state.cart[itemIndex].cartTotalQuantity += 1;
-      }else{
-         const tempProduct = { ...action.payload, cartTotalQuantity : 1};
-         state.cart.push(tempProduct);
+      if (existingIndex >= 0) {
+        state.cartItems[existingIndex] = {
+          ...state.cartItems[existingIndex],
+          cartQuantity: state.cartItems[existingIndex].cartQuantity + 1,
+        }
+      } else {
+        let tempProductItem = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(tempProductItem);
       }
-      setStorLocal('cart', JSON.stringify(state.cart))
-     },
+      setStorLocal("cart", JSON.stringify(state.cartItems));
+    },
+    decreaseCart(state, action) {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
 
-     removeFromCart(state,action){
-     const nextCartItems =  state.cart.filter(cartItem => cartItem.id !== action.payload.id)
-     state.cart = nextCartItems;
+      if (state.cartItems[itemIndex].cartQuantity > 1) {
+        state.cartItems[itemIndex].cartQuantity -= 1;
 
-     setStorLocal('cart', JSON.stringify(state.cart))
-     },
-     decreaseCart(state, action){
-      const itemIndex = state.cart.findIndex(
-        cartItem => cartItem.id === action.payload.id
-      ) 
-      
-      if(state.cart[itemIndex].cartTotalQuantity > 1) {
-        state.cart[itemIndex].cartTotalQuantity -= 1
-      }else if(state.cart[itemIndex].cartTotalQuantity === 1){
-        const nextCartItems =  state.cart.filter(cartItem => cartItem.id !== action.payload.id)
-        state.cart = nextCartItems; 
+     
+      } else if (state.cartItems[itemIndex].cartQuantity === 1) {
+        const nextCartItems = state.cartItems.filter(
+          (item) => item.id !== action.payload.id
+        );
+
+        state.cartItems = nextCartItems;
+
+       
       }
-      localStorage.setItem('cart', JSON.stringify(state.cart))
-     },
-     clearCart(state, action){
-      state.cart = [];
-      setStorLocal('cart', JSON.stringify(state.cart))
 
-     },
-     getTotals(state, action){
-    let {total, quantity} =  state.cart.reduce((cartTotal, cartItem) => {
-       const { price, cartTotalQuantity} = cartItem; 
-       const itemTotal = price * cartTotalQuantity;
+      setStorLocal("cart", JSON.stringify(state.cartItems));
+    },
+    removeFromCart(state, action) {
+      state.cartItems.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          const nextCartItems = state.cartItems.filter(
+            (item) => item.id !== cartItem.id
+          );
 
-       cartTotal.total += itemTotal
-       cartTotal.quantity += cartTotalQuantity
+          state.cartItems = nextCartItems;
 
+        }
+        setStorLocal("cart", JSON.stringify(state.cartItems));
+        return state;
+      });
+    },
+    getTotals(state, action) {
+      let { total, quantity } = state.cartItems.reduce(
+        (cartTotal, cartItem) => {
+          const { price, cartQuantity } = cartItem;
+          const itemTotal = price * cartQuantity;
 
-       return cartTotal;
-      }, {
-        total: 0,
-        quantity:0
-      } )
+          cartTotal.total += itemTotal;
+          cartTotal.quantity += cartQuantity;
+
+          return cartTotal;
+        },
+        {
+          total: 0,
+          quantity: 0,
+        }
+      );
+      total = parseFloat(total.toFixed(2));
       state.cartTotalQuantity = quantity;
       state.cartTotalAmount = total;
-     }
-   }
+    },
+    clearCart(state, action) {
+      state.cartItems = [];
+      setStorLocal("cart", JSON.stringify(state.cartItems));
+    },
+  },
+});
 
-})
+export const { addToCart, decreaseCart, removeFromCart, getTotals, clearCart } =
+  cartSlice.actions;
 
-
-export const{addToCart, removeFromCart, decreaseCart, clearCart,getTotals} = carttSlice.actions;
-export default carttSlice.reducer;
+export default cartSlice.reducer;
